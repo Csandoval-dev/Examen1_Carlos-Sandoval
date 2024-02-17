126c60ec14b5037b370d10311221336ce6779677
Csandoval-dev <156733491+Csandoval-dev@users.noreply.github.com>
2024-02-16 20:23:40 -0600

    Modificacion go_router

--- lib/core/router/go_router.dart
+++ lib/core/router/go_router.dart
@@ -1,13 +1,14 @@
-// routes.dart
-import 'package:go_router/go_router.dart';
-import 'package:flutter_examen_programovil/screens/books_screen.dart';
-import 'package:flutter_examen_programovil/screens/book_detail_screen.dart';
 
-final router = GoRouter(
-  initialLocation: '/',
-  errorPageBuilder: (context, state) => MaterialPage(child: Text('Error: ${state.error}')),
-  routes: [
-    BooksScreen.router(),
-    BookDetailScreen.router(),
+import 'package:flutter_examen_programovil/screens/book_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (context, GoRouterState state) {
+        return BooksScreen();
+      }
+    ),
   ],
 );
